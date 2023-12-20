f58cd36a70b6c3de5c6e573066a3f7bbb813ab50
Greggart9 <retrievergolden498@gmail.com>
2023-12-20 22:48:40 +0100

    done for the night

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -8,41 +8,44 @@ const Product = () => {
         <div className='productContainer'>
             {/* THE FIRST TEXT */}
 
-            <div productFirst>
+            <div className='productFirst'>
                 <p>Shop Laptops and Computers Laptops</p>
             </div>
               
               {/* THE LAPTOP AND INFO */}
             <div className='productSecond'>
                 <div className=' productSecondLeft'>
-                    <img src='/image/' alt='' />
+                    <img src='/image/lapo7 1.png' alt='' />
 
                 </div>
 
                 <div className=' productSecondRight'>
-                    <h3></h3>
-                    <p></p>
-                    <p></p>
+                    <h3>Lenovo Thinkpad E14, Intel Core I5
+                        -1135g7, 8gb Ram, 256gb Ssd Black 
+                        (20ta000yue)
+                    </h3>
+                    <p id='productCode'>Product Code: <span style={{color: 'aqua'}}>0988272</span></p>
+                    <p>Brand: Lenovo</p>
 
-                    <h2></h2><span></span><span></span>
+                    <div className='productPrice'><h1 style={{color: 'white'}}>₦667,700</h1><span style={{color: 'white', textDecorationLine: 'line-through'}}>₦697,700</span><span id='youSave' style={{color: 'aqua'}}>You save ₦30,000</span></div>
 
                     <div className='productSecondRightBtn'>
-                        <p></p>
-                        <button></button>
-                        <p></p>
-                        <button></button>
+                        <p id='quatityBtn'>Quantity:</p>
+                        <button>-</button>
+                        <p id='number'>0</p>
+                        <button>+</button>
                     </div>
 
                     <div className='AddToCart'>
-                        <button></button>
-                        <button></button>
+                        <button id='addToCart'>ADD TO CART</button>
+                        <button id='buyNow'>BUY NOW</button>
                     </div>
 
                     <div className='productSecondRightDelivery'>
                         <div className='deliveryLeft'>
-                            <p></p>
-                            <p></p>
-                            <p></p>
+                            <p>Same Day Delivery</p>
+                            <p>Available at:</p>
+                            <p style={{paddingTop: '15px'}}>Share With Friends</p>
                             <div className='deliveryIcon'>
                                 <img src='' alt='' />
                                 <img src='' alt='' />
@@ -54,8 +57,8 @@ const Product = () => {
                         </div>
 
                         <div className='deliveryRight'>
-                        <p></p>/
-                        <p></p>
+                        <p style={{paddingRight: '8px'}}>Ibadan</p>/
+                        <p  style={{paddingLeft: '8px'}}>Lagos</p>
 
                         </div>
 
